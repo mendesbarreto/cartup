bebb6d68bce13ff668a859e34c237a90b0d69251
Douglas Mendes <mendes-barreto@live.com>
2018-02-08 21:29:54 -0200

    Try to make the log more beautiful

---
+++ lib/cart_log.rb
@@ -0,0 +1,16 @@
+require 'logger'
+
+# module CartBinaryUploader
+#   class CartLog
+#     class << self
+#       def self.log
+#         if @logger.nil?
+#           @logger = Logger.new STDOUT
+#           @logger.level = Logger::DEBUG
+#           @logger.datetime_format = '%Y-%m-%d %H:%M:%S '
+#         end
+#         @logger
+#       end
+#     end
+#   end
+# end
