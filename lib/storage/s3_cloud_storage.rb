e064e0c571c30844fdfa091db7be2ca2aa782711
Douglas Mendes <mendes-barreto@live.com>
2018-03-21 12:19:29 -0300

    Add s3 suport to cartup and closes #8

--- lib/storage/s3_cloud_storage.rb
+++ lib/storage/s3_cloud_storage.rb
@@ -1,5 +1,6 @@
 require 'aws-sdk-s3'
 require 'fileutils'
+require 'json'
 require 'yaml'
 require 'json'
 require 'cart_logger'
@@ -20,44 +21,53 @@ module CartBinaryUploader
                    framework_version)
       @region_name = region_name
       @bucket_secret_key = bucket_secrete_key
-      setupS3Global
+      @bucket_access_key = bucket_acess_key
+      setup_s3_global
       super(bucket_name, framework_name, framework_version)
     end
 
-    def setupS3Global
-      Aws.config.update({region: @region_name, credentials: Aws::Credentials.new(@bucket_access_key, @bucket_secret_key)})
+    def setup_s3_global
+      Aws.config = {
+          :access_key_id => @bucket_access_key,
+          :secret_access_key => @bucket_secret_key,
+          :region => @region_name,
+      }
     end
 
     def create_storage
-      @storage = Aws::S3::Resource.new(region: region_name)
+      @storage = Aws::S3::Resource.new(credentials: Aws::Credentials.new(@bucket_secret_key, @bucket_access_key),
+                                       region: @region_name)
     end
 
     def create_bucket
-      @bucket = @storage.bucket(@bucket_name).object(@bucket_secret_key)
+      @bucket = @storage.bucket(@bucket_name)
     end
 
-    def create_file(framework_name_source, framework_name_destination)
-      @bucket_object = Aws::S3::Object.new(key: file_path,
-                                            bucket_name: @bucket_name,
-                                            client: s3_client)
-      @bucket_object.presigned_url(:get, expires_in: 3.154e+8)
+    def create_file(file_path)
+      puts "Creating #{file_path} on s3"
+      @bucket_object = @bucket.object(file_path)
+      @bucket_object.upload_file("./#{file_path}", acl:'public-read')
+      puts "Object #{file_path} created with url #{@bucket_object.public_url}"
+      @bucket_object.public_url
     end
 
 
     def file_on_storage_cloud(file)
-      bucket_file = bucket.file file
-      !bucket_file.nil?
+      @bucket.objects.each do |object|
+        if object.key == file
+          return true
+        end
+      end
+      false
     end
 
     def download_config_json_file(from_file)
-      json_file = @bucket.file from_file
-      json_file.download from_file
-      json_file
+      @bucket.object(from_file)
     end
 
     def upload_json(json_path)
-      CartLogger.log_info 'Starting upload file to google cloud'
-      @bucket.create_file(json_path, json_path)
+      CartLogger.log_info 'Starting upload json file to s3 cloud'
+      create_file(json_path)
       CartLogger.log_info 'Uploaded complete'
     end
   end
